03e102064031e5ebd6e8d32f1708fe21e793c45f
fredsonchaves07 <fredsonchaves07@gmail.com>
2022-12-07 22:28:22 -0300

    feat: project initial configuration
    Signed-off-by: fredsonchaves07 <fredson.ribeiro@objective.com.br>

---
+++ ms-consolidacao/src/test/java/com/fredsonchaves07/msconsolidacao/MsConsolidacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredsonchaves07.msconsolidacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConsolidacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
