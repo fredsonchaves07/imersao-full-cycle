903e69406ce5be0a0f4fc70f6589ad0b78ff78a3
fredsonchaves07 <fredsonchaves07@gmail.com>
2022-12-10 14:50:41 -0300

    feat: up players data
    Signed-off-by: fredsonchaves07 <fredson.ribeiro@objective.com.br>

---
+++ ms-consolidacao/src/main/resources/db/migration/V2__up_data_players.sql
@@ -0,0 +1,9 @@
+INSERT INTO players (id, name, price) VALUES ('4876d14f-d998-4abf-96ef-89fd53185464', 'Cristiano Ronaldo', 10.00);
+INSERT INTO players (id, name, price) VALUES ('0b8f08d8-d871-4a42-b395-17d698f477db', 'De Bruyne', 10.00);
+INSERT INTO players (id, name, price) VALUES ('0c9ba4fb-4609-464d-9845-421ca1e1e3bd', 'Harry Kane', 10.00);
+INSERT INTO players (id, name, price) VALUES ('67fbf409-d94f-4858-8423-8043576cda05', 'Lewandowski', 10.00);
+INSERT INTO players (id, name, price) VALUES ('c7830b65-cf79-49b7-a878-82250fec1d94', 'Maguirre', 10.00);
+INSERT INTO players (id, name, price) VALUES ('64fb9c2f-a45b-4f96-9d8b-b127878ca6f3', 'Messi', 10.00);
+INSERT INTO players (id, name, price) VALUES ('0f463bea-1dbd-4765-b080-9f5f170b6ded', 'Neymar', 10.00);
+INSERT INTO players (id, name, price) VALUES ('5ce233a8-5cd8-4a85-8156-9ac255cf909e', 'Richarlison', 10.00);
+INSERT INTO players (id, name, price) VALUES ('c707bfa9-074e-4636-8772-633e4b56248d', 'Vinicius Junior', 10.00);
